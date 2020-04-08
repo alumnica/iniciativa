cc234555be10cd8ef6379975cedcbd11acd9251d
weniver <weniver@gmail.com>
2020-04-08 10:52:02 -0500

    Minor Fix

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/weniver/Repos/Alumnica/producción/iniciativa/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
